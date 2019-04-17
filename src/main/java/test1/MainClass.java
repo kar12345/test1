a9fe55e482e2b137dca075b922f24294e2c6ae33
HP <HP@10.0.0.244>
2019-04-16 20:17:20 -0400

    some sys outs

--- src/main/java/test1/MainClass.java
+++ src/main/java/test1/MainClass.java
@@ -3,7 +3,7 @@ package test1;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Main Class");
+		System.out.println("Main Class-- Learning Jenkins !!! Awhh !!");
 	}
 
 }
